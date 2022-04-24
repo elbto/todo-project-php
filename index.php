7728a0373680f27bb1c72d23e47506f4930c7c2f
Toufik El bouamri <elb.toufik@gmail.com>
2022-04-24 17:51:40 +0200

    added some css

--- index.php
+++ index.php
@@ -5,18 +5,26 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="public/css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+  <script defer src="public/js/index.js"></script>
   <title>Todo</title>
 </head>
 
 <body>
-  <header>
-    Header
-  </header>
-  <main class="content">
-    CONTENT
-  </main>
-  <footer>FOOTER</footer>
+  <div class="container">
+    <header>
+      <div class="logo">TodoList</div>
+    </header>
+    <main class="content">
+      CONTENT
+    </main>
+    <footer>
+      <p>Todev 2022 © Tous droits réservés</p>
+    </footer>
+  </div>
 </body>
 
 </html>
